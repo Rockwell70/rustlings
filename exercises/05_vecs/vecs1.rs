7975a31a85bc27f24b868709018e821330f536c8
Rockwell70 <jrshabani@outlook.com>
2024-07-02 13:56:56 -0700

    exercise 5 vectors

--- exercises/05_vecs/vecs1.rs
+++ exercises/05_vecs/vecs1.rs
@@ -11,9 +11,9 @@
 
 fn array_and_vec() -> ([i32; 4], Vec<i32>) {
     let a = [10, 20, 30, 40]; // a plain array
-    let v = // TODO: declare your vector here with the macro for vectors
+    let v = vec![50, 60, 70, 71];
 
-    (a, v)
+    return (a, v)
 }
 
 #[cfg(test)]
