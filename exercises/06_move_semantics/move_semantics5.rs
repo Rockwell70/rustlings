81577eb5d63ec4f3f0a1772f0f2a56aafe92ba7c
Jeff Shabani <jrshabani@outlook.com>
2024-07-04 16:51:29 +0000

    mutable referencs

--- exercises/06_move_semantics/move_semantics5.rs
+++ exercises/06_move_semantics/move_semantics5.rs
@@ -11,8 +11,8 @@
 #[test]
 fn main() {
     let mut x = 100;
-    let y = &mut x;
-    let z = &mut x;
+    let y = &x;
+    let mut z = &mut x;
     *y += 100;
     *z += 1000;
     assert_eq!(x, 1200);
