15117b84393fd24da0c2ce761057bad400691232
Jeff Shabani <jrshabani@outlook.com>
2024-07-18 23:54:03 +0000

    string work

--- exercises/08_enums/enums3.rs
+++ exercises/08_enums/enums3.rs
@@ -5,26 +5,24 @@
 // Execute `rustlings hint enums3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 enum Message {
     // TODO: implement the message variant types based on their usage below
-    Move { x: i32, y: i32 },
+    ChangeColor(u8, u8, u8),
     Echo(String),
-    ChangeColor(i32, i32, i32),
-    Quit,
+    Move{x: u8, y: u8},
+    Quit
 }
 
 struct Point {
     x: u8,
-    y: u8,
+    y: u8
 }
 
 struct State {
     color: (u8, u8, u8),
     position: Point,
-    quit: bool,
-    message: String,
+    quit: bool
 }
 
 impl State {
@@ -36,8 +34,8 @@ impl State {
         self.quit = true;
     }
 
-    fn echo(&mut self, s: String) {
-        self.message = s
+    fn echo(&self, s: String) {
+        println!("{}", s);
     }
 
     fn move_position(&mut self, p: Point) {
@@ -45,20 +43,12 @@ impl State {
     }
 
     fn process(&mut self, message: Message) {
+        // TODO: create a match expression to process the different message variants
         match message {
-            Message::ChangeColor(r, g, b) => {
-                self.change_color((
-                    r.try_into().unwrap(),
-                    g.try_into().unwrap(),
-                    b.try_into().unwrap(),
-                ));
-            }
-            Message::Echo(str) => self.echo(str),
-            Message::Move { x, y } => self.move_position(Point{
-                x: x.try_into().unwrap(),
-                y: y.try_into().unwrap(),
-        }),
-            Message::Quit => self.quit(),
+            Message::ChangeColor(r, g, b) => self.change_color((r, g, b)),
+            Message::Echo(s) => self.echo(s),
+            Message::Move{x, y} => self.move_position(Point{x, y}),
+            Message::Quit => self.quit()
         }
     }
 }
@@ -69,21 +59,20 @@ mod tests {
 
     #[test]
     fn test_match_message_call() {
-        let mut state = State {
+        let mut state = State{
             quit: false,
-            position: Point { x: 0, y: 0 },
-            color: (0, 0, 0),
-            message: "hello world".to_string(),
+            position: Point{ x: 0, y: 0 },
+            color: (0, 0, 0)
         };
         state.process(Message::ChangeColor(255, 0, 255));
-        state.process(Message::Echo(String::from("Hello world!")));
-        state.process(Message::Move(Point { x: 10, y: 15 }));
+        state.process(Message::Echo(String::from("hello world")));
+        state.process(Message::Move{ x: 10, y: 15 });
         state.process(Message::Quit);
 
         assert_eq!(state.color, (255, 0, 255));
         assert_eq!(state.position.x, 10);
         assert_eq!(state.position.y, 15);
         assert_eq!(state.quit, true);
-        assert_eq!(state.message, "Hello world!");
     }
+
 }
